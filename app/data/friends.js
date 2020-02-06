e46cc71fbe643692003a0bc0cdec45c39f448fa7
Kate Davis <katedavis@kates-mbp.attlocal.net>
2020-02-06 03:11:13 -0600

    added people

--- app/data/friends.js
+++ app/data/friends.js
@@ -12,11 +12,150 @@ var friends = [
             2,
             5,
             4,
-            1
+            1,
         ]
-    }
 
+    },
+    {
+        "name": "Timothee",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            1,
+            3,
+            4,
+            5,
+            2,
+            3,
+            1,
+            4,
+            4,
+            5,
+        ]
 
+    },
+    {
+        "name": "Mary",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            3,
+            4,
+            2,
+            5,
+            5,
+            2,
+            2,
+            5,
+            3,
+            1,
+        ]
+
+    },
+    {
+        "name": "John",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            2,
+            5,
+            1,
+            1,
+            4,
+            3,
+            3,
+            5,
+            4,
+            2,
+        ]
+
+    },
+    {
+        "name": "Karen",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            3,
+            5,
+            2,
+            1,
+            5,
+            5,
+            5,
+            1,
+            2,
+            1,
+        ]
+
+    },
+
+    {
+        "name": "Chris",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            3,
+        ]
+
+    },
+    {
+
+        "name": "Kimberly",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            2,
+        ]
+
+    },
+    {
+
+        "name": "Mark",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            5,
+            1,
+            3,
+            3,
+            3,
+            1,
+            1,
+            5,
+            5,
+            3,
+        ]
+
+    },
+    {
+
+        "name": "Emily",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            2,
+            4,
+            4,
+            1,
+            1,
+            5,
+            2,
+            5,
+            5,
+            1,
+        ]
+
+    },
 
 
 
